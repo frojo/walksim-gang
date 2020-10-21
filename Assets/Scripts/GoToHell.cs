137d96cbac5f85054827716b333e4441f831ff67
Francisco Rojo <fran.mo.rojo@gmail.com>
2020-10-20 20:09:04 -0400

    more work on hell transition

--- Assets/Scripts/GoToHell.cs
+++ Assets/Scripts/GoToHell.cs
@@ -6,28 +6,55 @@ public class GoToHell : MonoBehaviour
     private PlayerInput input;
     private bool inHell = false;
 
+    private bool inTransition = false;
+    private bool startTransition = false;
+
+    private int test = 69;
+    private FirstPersonDrifter fpdtest;
+
     // Start is called before the first frame update
     void Start()
     {
       print("started being a player");
       input = GetComponent<PlayerInput>();
+      fpdtest = GetComponent<FirstPersonDrifter>();
+      print(input);
+      print(fpdtest);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+      print("startTransition = " + startTransition);
+      print("inTransition = " + inTransition);
+      if (startTransition) {
+        startTransition = false; 
+        print("starting transition!!!");
+
+      }
+
+      
     }
 
     public void OnFlip(InputAction.CallbackContext ctx)
     {
-      print("flip!!");
+      // for some reason, input is Null in this event handler, but
+      // we can call GetComponent() to get it /shrug
+      inHell = !inHell;
       // HEY WE MIGHT NOT HAVE THE VARIABLES FROM THE CURRENT CLASS
       // we might have to just use ctx
       //print(input.active);
 
-      // what a weird fucking word
-      //input.PassivateInput();
+      print("inTransition (flip)= " + inTransition);
+      // only activate on mouse button released
+      if (ctx.canceled && !inTransition) {
+        print("flip!!");
+        inTransition = true;
+        startTransition = true;
+
+      }
+      print("inTransition (flip after)= " + inTransition);
 
       // disable inputs
       // turn off colliders
